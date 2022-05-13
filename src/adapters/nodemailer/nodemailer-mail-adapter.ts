6967ed9ef165297f6db60b56918f644b59dd8d47
elisioWander <elisio741@hotmail.com>
2022-05-13 01:22:30 -0300

    change-email

--- src/adapters/nodemailer/nodemailer-mail-adapter.ts
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -2,19 +2,19 @@ import { MailAdapter, SendMailData } from "../mail-adapter";
 import nodemailer from 'nodemailer'
 
 const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
+  host: "smtp.office365.com",
+  port: 587,
   auth: {
-    user: "8ba86c4730bc17",
-    pass: "13b5cebf0f6a9a"
+    user: process.env.USER_EMAIL,
+    pass: process.env.USER_PASSWORD
   }
 });
 
 export class NodemailerMailAdapter implements MailAdapter {
   async sendMail({subject, body}: SendMailData) {
     await transport.sendMail({
-      from: 'Equipe Fidget <oi@fidget.com>',
-      to: 'Elisio Wander <elisio741@hotmail.com>',
+      from: process.env.USER_EMAIL,
+      to: process.env.RECEIVE_EMAIL,
       subject,
       html: body,
     })
