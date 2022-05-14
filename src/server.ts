2c76c97d5cb98900213420c888ac3b5870d18915
elisioWander <elisio741@hotmail.com>
2022-05-14 19:15:26 -0300

    cors-access-configuration

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
-import express from "express"
+import express, { application } from "express"
 import cors from 'cors'
 import { routes } from "./routes"
 
 const app = express()
 
-app.use(cors())
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", 'GET, PUT, POST, DELETE')
+
+  app.use(cors())
+  next()
+})
+
 app.use(express.json())
 app.use(routes)
 
