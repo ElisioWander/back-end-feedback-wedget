f32744739abc1449aebd85226b94f4cb60155b99
elisioWander <elisio741@hotmail.com>
2022-05-14 21:07:43 -0300

    nodemailer-config-to-send-email-to-mailtrap

--- src/adapters/nodemailer/nodemailer-mail-adapter.ts
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -2,8 +2,8 @@ import { MailAdapter, SendMailData } from "../mail-adapter";
 import nodemailer from 'nodemailer'
 
 const transport = nodemailer.createTransport({
-  host: "smtp.office365.com",
-  port: 587,
+  host: "smtp.mailtrap.io",
+  port: 25,
   auth: {
     user: process.env.USER_EMAIL,
     pass: process.env.USER_PASSWORD
@@ -13,7 +13,7 @@ const transport = nodemailer.createTransport({
 export class NodemailerMailAdapter implements MailAdapter {
   async sendMail({subject, body}: SendMailData) {
     await transport.sendMail({
-      from: process.env.USER_EMAIL,
+      from: 'Feedback-Wedget team <fedbackWedget@mailtrap.com>',
       to: process.env.RECEIVE_EMAIL,
       subject,
       html: body,
